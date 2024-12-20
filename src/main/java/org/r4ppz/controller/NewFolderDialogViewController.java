5ef82cb8ab576cf591d22016968821ac553eca54
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-20 21:05:19 +0800

    Add error handling to NewFolderDialogViewController for empty folder names

--- src/main/java/org/r4ppz/controller/NewFolderDialogViewController.java
+++ src/main/java/org/r4ppz/controller/NewFolderDialogViewController.java
@@ -1,14 +1,18 @@
 package org.r4ppz.controller;
 
 import org.r4ppz.util.HandleFile;
+import org.r4ppz.view.ErrorDialogView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class NewFolderDialogViewController {
     private HandleFile handleFile = HandleFile.getInstanceHandleFile();
+    private ErrorDialogView errorDialogView = ErrorDialogView.getInstancErrorAlertView();
 
     private MainViewController mainViewController;
 
@@ -24,20 +28,32 @@ public class NewFolderDialogViewController {
 
     @FXML
     public void createButtonAction(ActionEvent actionEvent) throws Exception {
+        Stage ownerStage = (Stage) ((Node) actionEvent.getSource()) .getScene().getWindow();
         String folderName = folderNameTextField.getText();
-        handleFile.createFolder(folderName);
-        mainViewController.refresh();
 
-        createButton.getScene().getWindow().hide();
+        if (folderName != null  && !folderName.isEmpty()) {
+            handleFile.createFolder(folderName);
+            mainViewController.refresh();
+    
+            createButton.getScene().getWindow().hide();
+        } else {
+            errorDialogView.showErrorDialogView(ownerStage);
+        }
     }
 
     @FXML
     public void folderNameTextFieldAction(ActionEvent actionEvent) throws Exception {
+        Stage ownerStage = (Stage) ((Node) actionEvent.getSource()) .getScene().getWindow();
         String folderName = folderNameTextField.getText();
-        handleFile.createFolder(folderName);
-        mainViewController.refresh();
 
 
-        createButton.getScene().getWindow().hide();
+        if (folderName != null  && !folderName.isEmpty()) {
+            handleFile.createFolder(folderName);
+            mainViewController.refresh();
+    
+            createButton.getScene().getWindow().hide();
+        } else {
+            errorDialogView.showErrorDialogView(ownerStage);
+        }
     }
 }
