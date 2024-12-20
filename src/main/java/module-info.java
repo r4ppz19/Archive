daf9eef814b2fe8ef408ab0aa30391950aea43fe
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-20 19:40:50 +0800

    Update pom.xml to set Java source and target version to 17; modify main class reference for consistency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.r4ppz {
     requires javafx.controls;
     requires javafx.fxml;
-    //requires javafx.swing;
     requires transitive javafx.graphics;
 
     requires org.apache.pdfbox;
@@ -10,12 +9,10 @@ module com.r4ppz {
     opens org.r4ppz to javafx.fxml;
     opens org.r4ppz.controller to javafx.fxml;
 
-    // open all packages
     exports org.r4ppz;
     exports org.r4ppz.controller;
     exports org.r4ppz.util;
     exports org.r4ppz.model;
-
-    // open test
+    
     exports org.r4ppz.test;
 }
