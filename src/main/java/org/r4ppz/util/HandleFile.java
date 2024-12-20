a56828963fbe041c038a39b39035bf0a90d3e397
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-20 09:31:32 +0800

    Add file chooser and file copy functionality to HandleFile class

--- src/main/java/org/r4ppz/util/HandleFile.java
+++ src/main/java/org/r4ppz/util/HandleFile.java
@@ -23,12 +23,25 @@ public class HandleFile {
     }
 
 
+    /**
+     * Opens a file chooser dialog for the user to select a file.
+     *
+     * @param stage the stage on which the file chooser dialog will be displayed
+     * @return the selected file, or null if no file was selected
+     */
     public File fileChooser(Stage stage) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select File");
         return fileChooser.showOpenDialog(stage);
     }
 
+    /**
+     * Copies a file to a specified destination path within the project.
+     *
+     * @param sourceFile the file to be copied
+     * @param destinationPath the path where the file should be copied to
+     * @throws IOException if an I/O error occurs during the copying process
+     */
     public void copyFileToProject(File sourceFile, String destinationPath) throws IOException {
         File destinationFile = new File(destinationPath, sourceFile.getName());
         Files.copy(sourceFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
