25b3d39410700385328c85f41ab8839c5d6e00e2
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-20 08:48:27 +0800

    Refactor MainViewV2Controller to replace fileContainer with leftPanelVbox and implement loadFolderToButtons method for folder listing

--- src/main/java/org/r4ppz/controller/MainViewV2Controller.java
+++ src/main/java/org/r4ppz/controller/MainViewV2Controller.java
@@ -11,7 +11,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,7 +20,7 @@ import javafx.stage.Stage;
 public class MainViewV2Controller {
 
     @FXML
-    private VBox fileContainer;
+    private VBox leftPanelVbox;
     @FXML
     private ImageView pdfImageView;
 
@@ -49,8 +48,9 @@ public class MainViewV2Controller {
     }
 
     private void refresh() {
-        fileContainer.getChildren().clear();
-        loadFilesToButton();
+        leftPanelVbox.getChildren().clear();
+        // loadFilesToButton();
+        loadFolderToButtons();
     }
 
     private void loadFilesToButton() {
@@ -68,17 +68,48 @@ public class MainViewV2Controller {
                 folderIcon.setFitWidth(22);
                 folderContainerButton.setGraphic(folderIcon);
                 folderContainerButton.getStyleClass().add("content-folder-button");
-                folderContainerButton.setContentDisplay(ContentDisplay.LEFT);
 
                 // Add tooltip
                 Tooltip fileNametooltip = new Tooltip(fileName);
                 folderContainerButton.setTooltip(fileNametooltip);
                 fileNametooltip.getStyleClass().add("file-name-tooltip");
 
-                fileContainer.getChildren().add(folderContainerButton);
+                leftPanelVbox.getChildren().add(folderContainerButton);
             }
         } else {
             System.out.println("Directory not found: " + directory.getAbsolutePath());
         }
     }
+
+    private void loadFolderToButtons() {
+        File mainFolder = new File("src/main/resources/org/r4ppz/uploads/");
+
+        if (mainFolder.isDirectory()) {
+            File[] filesInside = mainFolder.listFiles();
+            if (filesInside != null) {
+                for (File files : filesInside) {
+                    if (files.isDirectory()) {
+                        String folderName = files.getName();
+                        Button folderButton = new Button(folderName);
+
+                        ImageLoader imageLoader = ImageLoader.getInstanceImageLoader();
+                        Image folderImage = imageLoader.loadImage("/org/r4ppz/image/folder-icon.png");
+                        
+                        ImageView folderIcon = new ImageView(folderImage);
+                        folderIcon.setFitHeight(22);
+                        folderIcon.setFitWidth(22);
+                        folderButton.setGraphic(folderIcon);
+                        folderButton.getStyleClass().add("content-folder-button");
+
+                        leftPanelVbox.getChildren().add(folderButton);
+                    }
+                }
+            } else {
+                System.out.println("Files inside is null");
+            }
+        } else {
+            System.out.println("Main Folder is null");
+        }
+    }
+
 }
