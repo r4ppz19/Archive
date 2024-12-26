e6bb291f04fa7f7cb9ebb1d8e7975c12af337ad6
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-26 13:38:42 +0800

    Refactor HomePageViewController by removing unused imports; update DynamicHomePageDesign.css to set min and max dimensions for buttons; clean up HomePage.fxml by removing redundant button elements for improved clarity and organization.

--- src/main/java/org/r4ppz/controller/main/HomePageViewController.java
+++ src/main/java/org/r4ppz/controller/main/HomePageViewController.java
@@ -17,7 +17,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
