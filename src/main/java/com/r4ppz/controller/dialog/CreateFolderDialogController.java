ea1d6b566ebfafdda3d5cd5c87271e04b7468d9d
John Rey Rabosa <johnreyrabosa.f@gmail.com>
2025-01-06 22:25:52 +0800

    Make reference variable final, remove button variable and clean up FXML

--- src/main/java/com/r4ppz/controller/dialog/CreateFolderDialogController.java
+++ src/main/java/com/r4ppz/controller/dialog/CreateFolderDialogController.java
@@ -7,21 +7,17 @@ import com.r4ppz.view.dialog.SuccessDialogView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class CreateFolderDialogController {
 
-    private SuccessDialogView successDialogView = SuccessDialogView.getInstance();
-    private ErrorDialogView errorDialogView = ErrorDialogView.getInstance();
-    private FileHandler fileHandler = FileHandler.getInstance(successDialogView);
-    private ValidateFolderName validateFolderName = ValidateFolderName.getInstance(fileHandler, errorDialogView);
+    private final SuccessDialogView successDialogView = SuccessDialogView.getInstance();
+    private final ErrorDialogView errorDialogView = ErrorDialogView.getInstance();
+    private final FileHandler fileHandler = FileHandler.getInstance(successDialogView);
+    private final ValidateFolderName validateFolderName = ValidateFolderName.getInstance(fileHandler, errorDialogView);
     
     @FXML
     private TextField folderNameTextField;
-
-    @FXML
-    private Button createButton;
 
     @FXML
     public void handleCreateButtonAction(ActionEvent actionEvent) throws Exception {
