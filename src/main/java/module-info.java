7e36fc6da17223c7678dfb1ca39771fa13a5282c
r4ppz <johnreyrabosa.f@gmail.com>
2025-03-02 18:52:42 +0800

    Remove unnecessary exports and opens for test package in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,12 +9,10 @@ module com.r4ppz {
     opens com.r4ppz to javafx.fxml;
     opens com.r4ppz.controller.dialog to javafx.fxml;
     opens com.r4ppz.controller.main to javafx.fxml;
-    opens com.r4ppz.test to javafx.fxml;
 
     exports com.r4ppz;
     exports com.r4ppz.util;
     exports com.r4ppz.model;
-    exports com.r4ppz.test;
     exports com.r4ppz.service;
     exports com.r4ppz.view.dialog;
     opens com.r4ppz.controller.stage to javafx.fxml;
